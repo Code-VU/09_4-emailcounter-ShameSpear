b171b8059939fe6d7f233b92fd80da4a61e9b490
Cameron Ellis <Cameron.Ellis@vu.com>
2023-03-24 13:40:22 -0500

    failed

--- counter.py
+++ counter.py
@@ -2,8 +2,23 @@ def countEmail():
     # This first line is provided for you
     name = input("Enter file:")
     if len(name) < 1 : name = "mbox-short.txt"
-    handle = open(name)
-        
+    fromDict = {}
+    with open(name) as file:
+        for line in file:
+            # line = line.strip
+
+            if line.startswith("From "):
+                sender = line.split()[1]
+                if sender not in fromDict:
+                    fromDict[sender] = 1
+                else:
+                    fromDict[sender] += 1
+    # print(emails)
+    # print(max(fromDict), max(fromDict.values()))
+    dict_vals = fromDict.values()
+    max_val = max(dict_vals)
+
+    print(max(fromDict, key=fromDict.get), max_val)
 
 ## if you want to test locally run > python counter.py
 if __name__ == "__main__":
